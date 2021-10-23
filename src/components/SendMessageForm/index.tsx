10602b144e81cfe6fbf913c2d522380521ed9371
juanhdev <juanhlealbarros@gmail.com>
2021-10-23 14:43:17 -0300

    message form markup

--- src/components/SendMessageForm/index.tsx
+++ src/components/SendMessageForm/index.tsx
@@ -1,3 +1,37 @@
+import { useContext } from 'react';
+import { VscGithubInverted, VscSignOut } from 'react-icons/vsc';
+import { AuthContext } from '../../context/authContext';
+import styles from './styles.module.scss';
+
 export const SendMessageForm = () => {
-  return <h1>Send message form</h1>;
+  const { user } = useContext(AuthContext);
+
+  return (
+    <div className={styles.sendMessageFormWrapper}>
+      <button className={styles.signOutButton}>
+        <VscSignOut size={32} />
+      </button>
+
+      <header className={styles.userInfo}>
+        <div className={styles.userImg}>
+          <img src={user?.avatar_url} alt={`${user?.name} foto`} />
+        </div>
+        <strong className={styles.userName}>{user?.name}</strong>
+        <span className={styles.userGithub}>
+          <VscGithubInverted size={16} />
+          {user?.login}
+        </span>
+      </header>
+
+      <form className={styles.sendMessageForm}>
+        <label htmlFor="message">Mensagem</label>
+        <textarea
+          name="message"
+          id="message"
+          placeholder="Qual a sua expectativa para o evento?"
+        />
+        <button type="submit">Enviar mensagem</button>
+      </form>
+    </div>
+  );
 };
