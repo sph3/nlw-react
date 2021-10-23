c20cbfd2072b928fa21f77992490fe01b967b5bd
juanhdev <juanhlealbarros@gmail.com>
2021-10-22 22:18:32 -0300

    Import and use logo image

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,7 +1,10 @@
 import styles from './styles.module.scss';
+import logoImg from '../../assets/logo.svg';
 
 export const MessageList = () => {
-  return <div className={styles.messageListWrapper}>
-    
-  </div>;
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="Logo DoWhile 2021" />
+    </div>
+  );
 };
