9c5af4a4524b3735f51a5aaa0daff6b358fe7c57
juanhdev <juanhlealbarros@gmail.com>
2021-10-22 23:37:24 -0300

    Get code from URL

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc';
 import styles from './styles.module.scss';
 
@@ -5,6 +6,15 @@ export const LoginBox = () => {
   const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=${
     import.meta.env.VITE_CLIENT_ID
   }&redirect_uri=http://localhost:4500`;
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes('?code=');
+
+    if (hasGithubCode) {
+      const [, githubCode] = url.split('?code=');
+    }
+  }, []);
 
   return (
     <div className={styles.loginBoxWrapper}>
