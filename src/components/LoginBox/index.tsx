22ebaff5040e9b3a6813dd24504402ceb60eefb1
juanhdev <juanhlealbarros@gmail.com>
2021-10-23 14:17:39 -0300

    Use auth context on login box

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,14 +1,14 @@
 import { useContext } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc';
-import { AuthProvider } from '../../context/authContext';
+import { AuthContext, AuthProvider } from '../../context/authContext';
 import styles from './styles.module.scss';
 
 export const LoginBox = () => {
-  useContext;
+  const { signInUrl, user } = useContext(AuthContext);
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href={} className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size="24" />
         Entrar com Github
       </a>
