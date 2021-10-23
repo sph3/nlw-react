9e09cd83188797fce0f5c31b96233d5f75cc3f18
juanhdev <juanhlealbarros@gmail.com>
2021-10-23 14:02:33 -0300

    Create types and sign in github

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,11 +1,31 @@
 import { useEffect } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc';
+import { api } from '../../services/API';
 import styles from './styles.module.scss';
+
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  };
+};
 
 export const LoginBox = () => {
   const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=${
     import.meta.env.VITE_CLIENT_ID
   }&redirect_uri=http://localhost:4500`;
+
+  const signIn = async (githubCode: string) => {
+    const res = await api.post<AuthResponse>('auth', { code: githubCode });
+
+    const { token, user } = res.data;
+    localStorage.setItem('dowhile:token', token);
+
+    console.log(user);
+  };
 
   useEffect(() => {
     const url = window.location.href;
@@ -14,6 +34,7 @@ export const LoginBox = () => {
     if (hasGithubCode) {
       const [urlWIthoutCode, githubCode] = url.split('?code=');
       window.history.pushState({}, '', urlWIthoutCode);
+      signIn(githubCode);
     }
   }, []);
 
