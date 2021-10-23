b867a910907dfa1c4c4edd20c86c37b807c35ee8
juanhdev <juanhlealbarros@gmail.com>
2021-10-22 23:13:08 -0300

    Login button redirect sign in github

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -2,10 +2,12 @@ import { VscGithubInverted } from 'react-icons/vsc';
 import styles from './styles.module.scss';
 
 export const LoginBox = () => {
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=http://localhost:4500`;
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size="24" />
         Entrar com Github
       </a>
