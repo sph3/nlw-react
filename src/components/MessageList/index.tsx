68a99dc54987ca8043891294d1c13db4af33081b
juanhdev <juanhlealbarros@gmail.com>
2021-10-22 22:26:26 -0300

    Create template for messageList

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -5,6 +5,53 @@ export const MessageList = () => {
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="Logo DoWhile 2021" />
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImg}>
+              <img
+                src="https://github.com/diego3g.png"
+                alt="Diego Fernandes foto"
+              />
+            </div>
+            <span>Diego Fernandes</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImg}>
+              <img
+                src="https://github.com/diego3g.png"
+                alt="Diego Fernandes foto"
+              />
+            </div>
+            <span>Diego Fernandes</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImg}>
+              <img
+                src="https://github.com/diego3g.png"
+                alt="Diego Fernandes foto"
+              />
+            </div>
+            <span>Diego Fernandes</span>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 };
