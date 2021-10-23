cb85f57ea46e4922d57cf6c63ef1e712ac07c84e
juanhdev <juanhlealbarros@gmail.com>
2021-10-23 14:09:37 -0300

    Create types

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,11 +1,21 @@
 import { createContext, FC } from 'react';
 
+type User = {
+  id: string;
+  name: string;
+  login: string;
+  avatar_url: string;
+};
+
+type AuthContextData = {
+  user: User | null;
+  signInUrl: string;
+};
+
 const AuthContext = createContext(null);
 
 export const AuthProvider: FC = (props) => {
   return (
-      <AuthContext.Provider value={null}>
-        {props.children}
-      </AuthContext.Provider>
+    <AuthContext.Provider value={null}>{props.children}</AuthContext.Provider>
   );
 };
