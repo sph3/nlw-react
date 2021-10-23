6afb3bca31e643cc94c89ba3a3c1574aa6db00eb
juanhdev <juanhlealbarros@gmail.com>
2021-10-22 22:04:39 -0300

    Import components into app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { LoginBox } from './components/LoginBox';
+import { MessageList } from './components/MessageList';
 import styles from './styles/App.module.scss';
 
 export function App() {
   return (
     <main className={styles.contentWrapper}>
-      <h1>Hello, World!</h1>
+      <MessageList />
+      <LoginBox />
     </main>
   );
 }
