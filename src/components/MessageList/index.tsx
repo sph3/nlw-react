14185505eadbdbe1fafc1657c4321299055b7261
juanhdev <juanhlealbarros@gmail.com>
2021-10-22 22:47:14 -0300

    Get messages from API

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,7 +1,15 @@
+import { api } from '../../services/api';
 import styles from './styles.module.scss';
 import logoImg from '../../assets/logo.svg';
+import { useEffect } from 'react';
 
 export const MessageList = () => {
+  useEffect(() => {
+    api.get('messages/last3').then((res) => {
+      console.log(res.data);
+    });
+  }, []);
+
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="Logo DoWhile 2021" />
